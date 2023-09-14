fa0c1074db62e267b87522e6ea575c7f6b13a332
Mohammad Aref Tamanadar (sn1per) <arefttt@users.noreply.github.com>
2023-09-14 14:32:55 +0330

    Update FilzaArduino.h

--- FilzaArduino.h
+++ FilzaArduino.h
@@ -13,17 +13,24 @@ public:
   bool remove();
   bool open(uint8_t mode);
   void close();
-  void save(float value);
-  float read();
+  void save(float value); // Save a float value
+  void save(const String &value); // Save a string value
+  float readFloat(); // Read a float value
+  String readString(); // Read a string value
   void writeLine(const String &line);
   String readLine();
   void writeBytes(const uint8_t *buffer, size_t size);
   size_t readBytes(uint8_t *buffer, size_t size);
+  void appendLine(const String &line); // New method for appending a line to the file
+  void move(const char *newPath); // New method for moving the file
+  void rename(const char *newName); // New method for renaming the file
 
 private:
   const char *path;
   const char *filename;
   File file;
+
+  void debugPrint(const char *message);
 };
 
 #endif
