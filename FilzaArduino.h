8e9f5b9153dba6488d17e026dbb8e3db7c926c99
Mohammad Aref Tamanadar (sn1per) <arefttt@users.noreply.github.com>
2023-09-14 16:05:49 +0330

    Update FilzaArduino.h

--- FilzaArduino.h
+++ FilzaArduino.h
@@ -1,36 +1,63 @@
 #ifndef FILZA_ARDUINO_H
 #define FILZA_ARDUINO_H
 
-#include <Arduino.h>
 #include <SD.h>
+#include <CRC32.h>
+
+enum ErrorCode {
+  SUCCESS = 0,
+  FILE_NOT_FOUND,
+  FILE_OPEN_FAILED,
+  FILE_READ_FAILED,
+  FILE_WRITE_FAILED,
+};
 
 class FilzaArduino {
 public:
   FilzaArduino(const char *path, const char *filename);
   ~FilzaArduino();
-
+  
   bool exists();
   bool remove();
   bool open(uint8_t mode);
   void close();
-  void save(float value); // Save a float value
-  void save(const String &value); // Save a string value
-  float readFloat(); // Read a float value
-  String readString(); // Read a string value
+  
+  void save(float value);
+  void save(const String &value);
+  
+  float readFloat();
+  String readString();
+  
   void writeLine(const String &line);
   String readLine();
+  
   void writeBytes(const uint8_t *buffer, size_t size);
   size_t readBytes(uint8_t *buffer, size_t size);
-  void appendLine(const String &line); // New method for appending a line to the file
-  void move(const char *newPath); // New method for moving the file
-  void rename(const char *newName); // New method for renaming the file
+  
+  void appendLine(const String &line);
+  
+  void move(const char *newPath);
+  void rename(const char *newName);
+  
+  bool createDirectory(const char *path);
+  bool removeDirectory(const char *path);
+  
+  void listDirectory(const char *dirname, int numTabs = 0);
+  
+  void changeDirectory(const char *path);
+  
+  size_t getFileSize(const char *filename);
+  
+  uint32_t calculateChecksum(const char *filename);
+  
+  String getFileType(const char *filename);
 
 private:
+  File file;
   const char *path;
   const char *filename;
-  File file;
-
+  
   void debugPrint(const char *message);
 };
 
-#endif
+#endif // FILZA_ARDUINO_H
